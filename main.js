9e4dae60e3dee58e8cf7cb81c61320346aceef75
Yura <61066244+Yura1994@users.noreply.github.com>
2020-06-23 01:16:01 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+// parallax Фон
+function parallax(event){
+    this.querySelectorAll('.layer').forEach(layer => {
+    let speed = layer.getAttribute('data-speed');
+    layer.style.transform = `translateX(${event.clientX * speed/9000}px)`
+    });
+    }
+    
+    document.addEventListener('mousemove', parallax)
+
+
+// Получить случайное число от 0 до size-1
+var getRandomNumber = function (size) {
+    return Math.floor(Math.random() * size);
+    };
+    // Вычислить расстояние от клика (event) до клада (target)
+    var getDistance = function (event, target) {
+    var diffX = event.offsetX - target.x;
+    var diffY = event.offsetY - target.y;
+    return Math.sqrt((diffX * diffX) + (diffY * diffY));
+    };
+    // Получить для расстояния строку подсказки
+    var getDistanceHint = function (distance) {
+  
+    if (distance < 10) {
+    return "Обожжешься!";
+    } else if (distance < 20) {
+    return "Очень горячо";
+    } else if (distance < 40) {
+    return "Горячо";
+    } else if (distance < 80) {
+    return "Тепло";
+    } else if (distance < 160) {
+    return "Холодно";
+    } else if (distance < 320) {
+    return "Очень холодно";
+    } else {
+    return "Замерзнешь!";
+    }
+    };
+    // Создаем переменные
+    var width = 400;
+    var height = 400;
+    var clicks = 0;
+    // Случайная позиция клада
+    var target = {
+    x: getRandomNumber(width),
+    y: getRandomNumber(height)
+    };
+    // Добавляем элементу img обработчик клика
+    document.querySelector("#map").addEventListener("click", (event) => {
+    clicks++;
+    //console.log(clicks);
+    // Получаем расстояние от места клика до клада
+    var distance = getDistance(event, target);
+    // Преобразуем расстояние в подсказку
+    var distanceHint = getDistanceHint(distance);
+    // Записываем в элемент #distance новую подсказку
+    document.querySelector("#distance").textContent = distanceHint;
+    // Если клик был достаточно близко, поздравляем с победой
+    if (distance < 8) {
+    alert("Клад найден! Сделано кликов: " + clicks);
+    }
+    });
+    
+    
